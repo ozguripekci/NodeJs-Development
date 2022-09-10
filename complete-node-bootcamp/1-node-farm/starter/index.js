741e0ae2eabf12c56b7ac5de5d2ff1687b93f24d
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-09-10 15:53:35 +0200

    handling api

--- complete-node-bootcamp/1-node-farm/starter/index.js
+++ complete-node-bootcamp/1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-const { Server } = require('tls');
+const url = require('url');
 
 
 //! FILES
@@ -36,10 +36,36 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 
 //! SERVER
 
-http.createServer((req, res) => {
-    res.end('Hello from the Server');
+const data =fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8',);
+const dataObj = JSON.parse(data)
+
+
+const server = http.createServer((req, res) => {
+    // console.log(req)
+    console.log(req.url)
+
+    const pathName = req.url;
+    if (pathName === '/'  || pathName ==='/overview') {
+        res.end('This is the Overview page.');
+        
+    } else if (pathName === '/product') {
+        res.end('This is the Product page.');
+
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+
+    } else {
+        res.writeHead(404, { 
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world',
+        });
+        res.end('<h1>PAGE IS NOT FOUND! - 404 Error</h1>')
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Server has been started: on port 8000')
-})
+    console.log('Listening to requests on port 8000');
+});
+
+

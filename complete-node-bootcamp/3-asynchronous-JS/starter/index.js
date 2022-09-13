2e5aaae1b4f8f8df412d41d50bfa07a11b43ef28
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-09-13 07:59:39 +0200

    async/await

--- complete-node-bootcamp/3-asynchronous-JS/starter/index.js
+++ complete-node-bootcamp/3-asynchronous-JS/starter/index.js
@@ -27,21 +27,49 @@ const getDogPic = async () => {
         const data = await readFilePro(`${__dirname}/dog.txt`)
         console.log(`Breed: ${data}`);
     
-        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-        console.log(res.body.message);
+        const res1Pro = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        const res2Pro = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        const res3Pro = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        const all = await Promise.all([res1Pro, res2Pro, res3Pro])
+        const imgs = all.map(el => el.body.message)
+        console.log(imgs);
     
-        await writeFilePro('dog-image.txt', res.body.message)
+        await writeFilePro('dog-image.txt', imgs.join('\n'));
         console.log('Random dog image was successfully saved');
         
     } catch (err) {
         console.log(err);
+        throw(err);
     }
-
+    return '2:Ready🌝'
 
 }
 
+(async() => {
+    try {
+        console.log('1: Will get dog pics!');
+        const x = await getDogPic();
+        console.log(x);
+        console.log('3:DONE');
 
-getDogPic();
+    } catch (err) {
+        console.log('Error 🔥');
+    }
+})();
+
+
+
+
+/* console.log('1: Will get dog pics!');
+getDogPic()
+    .then(x => {
+        console.log(x);
+        console.log('3:DONE');
+    })
+    .catch(err => {
+        console.log('Error 🔥');
+    })
+ */
 
 
 /* readFilePro(`${__dirname}/dog.txt`)
